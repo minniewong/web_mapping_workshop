c663c6cd20b93ac70292ca40b4504e7af72b77d2
minniewong <min.m.wong@nasa.gov>
2016-05-02 17:19:00 -0400

    directions

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -109,9 +109,7 @@ function getDirections(frm, to){
         ],
     costing: 'pedestrian',
       units: 'miles'
-    
     })
-    
 	$.ajax({
     	url: 'https://valhalla.mapzen.com/route',
       	data: {
@@ -130,6 +128,7 @@ function getDirections(frm, to){
           	"stroke": "#ed23f1",
             "stroke-opacity": 0.8,
             "stroke-width": 8
+          }
         })
     })
 }
