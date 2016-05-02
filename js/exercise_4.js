cb210e9a78027b7a9e80db473cd153afb4186122
minniewong <min.m.wong@nasa.gov>
2016-05-02 15:22:37 -0400

    added popup

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -35,3 +35,11 @@ featureLayer.on('ready', function(){
     })
 	map.fitBounds(featureLayer.getBounds());
 })
+
+featureLayer.on('ready', function(){
+	this.eachLayer(function(layer){
+    layer.bindPopup('Welcome to ' + layer.feature.properties.name)
+    
+    })
+
+})
