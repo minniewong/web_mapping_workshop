0cee639de50bc895346befcfc69344717047b575
minniewong <min.m.wong@nasa.gov>
2016-05-02 14:35:21 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'minwong.019i8kk0';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoibWlud29uZyIsImEiOiJjaW02cDMxbGIwMnhkdWptM3FvZjlucXY2In0.Td3u5SJ7rGvsp_OAUrRggA';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
