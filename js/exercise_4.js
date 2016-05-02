135149a4704a745b22947949d1dcd72128f90b05
minniewong <min.m.wong@nasa.gov>
2016-05-02 16:26:02 -0400

    user location

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -74,4 +74,23 @@ featureLayer.on('ready', function(){
   $('#sidebar').fadeOut(200);
   
   })
+  
+var myLocation = L.mapbox.featureLayer().addTo(map);
+
+map.on('locationfound', function(e){})
+	myLocation.setGeoJSON({
+    	type: 'Feature', 
+     	 geometry: {
+       	 type: 'Point',
+       	 coordinates:[ e.latlng.lng, elatlng.lat ]
+         }, 
+      	properties: {
+          	"title": "Here I am!",
+          	"marker-color": "#ff8888",
+          	"marker-symbol": "star"
+        }
+    })
+})
+
+map.locate({setView: true})
                      
