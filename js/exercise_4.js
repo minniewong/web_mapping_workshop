900fcb800b124e124c920109944ea0885e04727e
minniewong <min.m.wong@nasa.gov>
2016-05-02 17:08:33 -0400

    directions

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -101,7 +101,7 @@ var routeLine = L.mapbox.featureLayer().addTo(map);
 
 function getDirections(frm, to){
 	var jsonPayload = JSON.stringify({
-    	locations: [
+    	locations:[
           {lat: frm[1], lon[0]},
           {lat: to[1], lon[0]}
         ],
