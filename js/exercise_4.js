d29005536d70b5f25c14a0e7099a2020703ccc2b
minniewong <min.m.wong@nasa.gov>
2016-05-02 17:41:57 -0400

    directions

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -106,6 +106,7 @@ function getDirections(frm, to){
           {lat: to[1], lon: to[0]}
         ],
     costing: 'pedestrian',
+      directions_options:{
       units: 'miles'
     })
 	$.ajax({
@@ -132,7 +133,7 @@ function getDirections(frm, to){
         $('#directions').fadeIn(400, function(){
           var summary = data.trip.summary
         	$('#summary').empty();
-          	$('#distance').text((Math.round(summary.length * 100) / 100) + data.trip.units);
+          	$('#distance').text((Math.round(summary.length * 100) / 100) + ' ' + data.trip.units);
           	$('#time').text((Math.round(summary.time / 60 * 100) / 100) + ' min');
         
         })
